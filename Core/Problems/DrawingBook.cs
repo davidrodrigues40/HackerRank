c7cecfc2a9d15f3f6d77966ddd76109f0a9fc704
David Rodrigues <davidrodrigues@quickenloans.com>
2021-04-14 17:37:09 -0400

    Drawing Book

---
+++ Core/Problems/DrawingBook.cs
@@ -0,0 +1,58 @@
+﻿using Core.Helpers;
+using System;
+
+namespace Core.Problems
+{
+    public class DrawingBook : IProblem
+    {
+        public void Solve(object input)
+        {
+            string[] p = input.ToStringList(' ');
+            int pages = p[0].ToInt();
+            int turnTo = p[1].ToInt();
+
+            Console.WriteLine(pageCount(pages, turnTo));
+        }
+
+        static int pageCount(int n, int p)
+        {
+            if (n / 2 >= p)
+                return turnFromFront(p);
+
+            return turnFromEnd(n, p);
+        }
+
+        static int turnFromEnd(int n, int p)
+        {
+            int turned = 0;
+            int mod = n % 2 == 0 ? 1 : 0;
+            if (p == n) return turned;
+
+            for (int i = n + mod; i >= p;)
+            {
+                if (i == p || i - 1 == p)
+                    break;
+                i -= 2;
+                turned++;
+            }
+
+            return turned;
+        }
+
+        static int turnFromFront(int p)
+        {
+            int turned = 0;
+            for (int i = 2; i <= p;)
+            {
+                turned++;
+                if (i == p || i + 1 == p)
+                    break;
+
+                i += 2;
+            }
+
+            return turned;
+        }
+
+    }
+}
---
+++ Core/Problems/SalesByMatch.cs
@@ -0,0 +1,40 @@
+﻿using Core.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace Core.Problems
+{
+    public class SalesByMatch : IProblem
+    {
+        public void Solve(object input)
+        {
+            string[] p = input.ToStringList(' ');
+            var ar = JsonConvert.DeserializeObject<int[]>(p[0]);
+            var n = int.Parse(p[1]);
+
+            Console.WriteLine(sockMerchant(n, ar));
+        }
+
+        static int sockMerchant(int n, int[] ar)
+        {
+            var socks = ar.ToList();
+            var pairs = 0;
+
+            for (int i = 0; i < socks.Count; i++)
+            {
+                for (int x = i + 1; x < socks.Count; x++)
+                {
+                    if (socks[i] == socks[x])
+                    {
+                        pairs++;
+                        socks.RemoveAt(x);
+                        break;
+                    }
+                }
+            }
+            return pairs;
+        }
+
+    }
+}
