cab11b32f8aa5e280df05a6f96a90fe21b698a46
David Rodrigues <davidrodrigues@rocketmortgage.com>
2023-05-25 14:57:42 -0400

    Finished Repeated Strings

--- Core/Problems/RepeatedString.cs
+++ Core/Problems/RepeatedString.cs
@@ -10,51 +10,10 @@ namespace Core.Problems
 		{
 			(string s, long n) = input.ToString_Long(',');
 
-			List<RepeatedResponse> responses = new();
-			_stopWatch.Start();
-			var value = DoWork(s, n);
-			_stopWatch.Stop();
-			responses.Add(new RepeatedResponse { name = "DoWork", value = value, time = _stopWatch.Elapsed.ToString() });
-
-			_stopWatch.Reset();
-			_stopWatch.Start();
-			value = MyWork(s, n);
-			_stopWatch.Stop();
-			responses.Add(new RepeatedResponse { name = "MyWork", value = value, time = _stopWatch.Elapsed.ToString() });
-
-			return responses;
+			return DoWork(s, n);
 		}
 
 		private static long DoWork(string s, long n)
-		{
-			long count = 0;
-			foreach (char c in s)
-			{
-				if (c == 'a')
-				{
-					count++;
-				}
-			}
-
-			if (n < s.Length)
-				return count;
-
-			long factor = (n / s.Length); // 3.3
-			long rem = (n % s.Length); // 1
-			count = factor * count; // 3 * 3 = 9
-			for (int i = 0; i < rem; i++)
-			{
-				if (s[i] == 'a')
-				{
-					count++;
-				}
-			}
-			_stopWatch.Stop();
-
-			return count;
-		}
-
-		private static long MyWork(string s, long n)
 		{
 
 			if (s.Length == 1)
@@ -78,12 +37,5 @@ namespace Core.Problems
 
 			return count;
 		}
-	}
-
-	internal class RepeatedResponse
-	{
-		public string name { get; set; }
-		public long value { get; set; }
-		public string time { get; set; }
 	}
 }
